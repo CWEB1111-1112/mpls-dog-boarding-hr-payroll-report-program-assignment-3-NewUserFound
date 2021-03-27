e212e8b48f5bd669fbf0e7eec1b05ab3c4b9d229
Lucas Meier <meiluca@dunwoody.edu>
2021-03-27 14:21:54 -0500

    Cant figure out how to arrange department lists so i can access them for output

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 /**
 Name: Lucas Meier
-Date of completion:
+Date of completion:N/A
 
 Please be sure to comment your code - provide a comment for each structure use in 
 program.
@@ -15,33 +15,35 @@ namespace assignment_three
         {   
             //main program calling inputs/outputs
             Console.WriteLine($"Welcome to MPLS Dog Boarding HR Payroll Report Program\nYou will be prompted to enter the Employee's Name, Department Number, Hourly Salary, and Number of Hours Worked");
-            inputs();
+            workingProgram();
+            //Payroll_Report();
             Console.WriteLine("Thank you for using MPLS Dog Boarding HR Payroll Report Program.");
         }
-        public static void inputs()
+        public static void workingProgram()
         {   
             //Declarations
+            string[] DEPARTMENT_ARRAY = {"Personnel","Marketing","Information Technology","Computer Services","Sales","Legal","Accounting"};
             string Employee_Name;
-            double Hourly_Salary, Number_Of_Hours_Worked;
-            int Department_Number;
-            double primer;
+            double Hourly_Salary, Number_Of_Hours_Worked, total_Gross_Salary;
+            int Department_Number, primer;
             bool end = false;
             bool valid_Number = false;
 
             //Initialize the primer
-            Console.WriteLine($"To begin please press the 1 key [1]");
+            Console.WriteLine("To enter employee Payroll data press the 1 key [1] else to exit enter the 2 key [2].");
             primer = Convert.ToInt32(Console.ReadLine());
 
             //looping structure
             while(end != true)
-            {   
-                //decision logic to continue iterating
-                if(primer == 1){
-                    //initializing declared variables
+            {
+                //initializing declared variables
+                if(primer == 1)
+                {
                     //loop to initialize Department number and validate
                     while(valid_Number == false)
                    {
-                        Console.WriteLine("Please Enter the Employee's Department Number\n[1] for Personnel\n[2] for Marketing\n[3] for Information Technology\n[4] for Computer Services\n[5] for Sales\n[6] for Legal\n[7] for Accounting");
+                        //input of user data for department
+                        Console.WriteLine($"Please Enter the Employee's Department Number\n[1] for {DEPARTMENT_ARRAY[0]}\n[2] for {DEPARTMENT_ARRAY[1]}\n[3] for {DEPARTMENT_ARRAY[2]}\n[4] for {DEPARTMENT_ARRAY[3]}\n[5] for {DEPARTMENT_ARRAY[4]}\n[6] for {DEPARTMENT_ARRAY[5]}\n[7] for {DEPARTMENT_ARRAY[6]}");
                         Department_Number = Convert.ToInt32(Console.ReadLine());
 
                         //passing the number entered through a new validate expression
@@ -60,38 +62,66 @@ namespace assignment_three
                             Console.WriteLine($"{e.GetType().Name}: {index} is an invalid Department Number, try again.");
                             valid_Number = false;
                         }
-                    } 
                     
-                    Console.WriteLine("Please Enter the Employee's name");
-                    Employee_Name = Console.ReadLine();     
+                        Console.WriteLine("Please Enter the Employee's name");
+                        Employee_Name = Console.ReadLine();     
 
-                    Console.WriteLine("Please Enter the Employee's Hourly Salary");
-                    Hourly_Salary = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Please Enter the Employee's Hourly Salary");
+                        Hourly_Salary = Convert.ToInt32(Console.ReadLine());
 
-                    Console.WriteLine("Please Enter the Employee's Number of Hours Worked");
-                    Number_Of_Hours_Worked = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Please Enter the Employee's Number of Hours Worked");
+                        Number_Of_Hours_Worked = Convert.ToInt32(Console.ReadLine());
 
-                    //reinitializing the primer
-                    Console.WriteLine("To continue entering employee Payroll data press the 1 key [1] else to exit enter the 2 key [2].");
+                        //equation for total gross salary
+                        total_Gross_Salary = Hourly_Salary*Number_Of_Hours_Worked;
+
+                        //Decision logic for each department list
+                        if(Department_Number == 1)
+                        {
+                            List<String> departmentPersonnel = new List<String>();
+                            departmentPersonnel.Add(Convert.ToString(total_Gross_Salary));
+                        }
+                        else if(Department_Number == 2)
+                        {
+                            List<String> departmentMarketing = new List<String>();
+                            departmentMarketing.Add(Convert.ToString(total_Gross_Salary));
+                        }
+                        else if(Department_Number == 3)
+                        {
+                            List<String> departmentInformation_Technology = new List<String>();
+                            departmentInformation_Technology.Add(Convert.ToString(total_Gross_Salary));
+                        }
+                        else if(Department_Number == 4)
+                        {
+                            List<String> departmentComputer_Services = new List<String>();
+                            departmentComputer_Services.Add(Convert.ToString(total_Gross_Salary));
+                        }
+                        else if(Department_Number == 5)
+                        {
+                            List<String> departmentSales = new List<String>();
+                            departmentSales.Add(Convert.ToString(total_Gross_Salary));
+                        }
+                        else if(Department_Number == 6)
+                        {
+                            List<String> departmentLegal = new List<String>();
+                            departmentLegal.Add(Convert.ToString(total_Gross_Salary));
+                        }
+                        else
+                        {
+                            List<String> departmentAccounting = new List<String>();
+                            departmentAccounting.Add(Convert.ToString(total_Gross_Salary));
+                        }
+                    }
+
+                    //initializing the primer
+                    Console.WriteLine("To enter employee Payroll data press the 1 key [1] else to exit enter the 2 key [2].");
                     primer = Convert.ToInt32(Console.ReadLine());
                     valid_Number = false;
-
-                    /*List<Payroll_Report_Input> all_Department_Reports = new List<Payroll_Report_Input>();
-
-                    string[] departmentNameArray = {"Personnel","Marketing","Information Technology","Computer Services","Sales","Legal","Accounting"};
-                    foreach(var department in all_Department_Reports)
-                    {
-                        total_Gross_Payroll = Hourly_Salary*Number_Of_Hours_Worked;
-                    }
-                    Console.WriteLine(departmentNameArray[Convert.ToInt32(departments-1)] + Convert.ToString(total_Gross_Payroll));
-                */}
+                }
                 else{
-                    //end of inputs
                     end = true;
                 }
-                
             }
-            
         }
     }
 }
